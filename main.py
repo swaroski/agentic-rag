fbdf3ed0928a60bfba4b1e7dba2afdd12f96b810
swaroski <bhise.suyog@gmail.com>
2025-03-09 19:19:11 -0400

    name change to file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import subprocess
 
 # Run the Streamlit app using a subprocess
 def start():
-    command = ["streamlit", "run", "agentic_rag_streamlit.py", "--server.port", os.getenv("PORT", "8501")]
+    command = ["streamlit", "run", "app.py", "--server.port", os.getenv("PORT", "8501")]
     subprocess.run(command)
 
 if __name__ == "__main__":
